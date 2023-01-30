eda50ea4d19772cc0ecc9aff5c48dafd9941dd43
Timothée Dione <timothee.dione@gmail.com>
2023-01-30 21:50:43 +0100

    advance on podcst creation request

--- laravel/app/Http/Requests/PodcastRequest.php
+++ laravel/app/Http/Requests/PodcastRequest.php
@@ -13,7 +13,7 @@ class PodcastRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,11 @@ class PodcastRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            "podcast_id" => "numeric",
+            "no" => "numeric",
+            "title" => "alpha_numeric",
+            "characters" => "array",
+            "tags" =>  'array',
         ];
     }
 }
