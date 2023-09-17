dfecdb0fe00ac21e77a675923799458745c047e6
Timothée Dione <timothee.dione@gmail.com>
2023-09-18 01:09:30 +0200

    token problem solved

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\RegisterRequest;
 use App\Http\Controllers\BaseController;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends BaseController
 {
@@ -19,21 +19,31 @@ class AuthController extends BaseController
             'email' => $request->input('email'),
             'password' => Hash::make($request->input('password')),
         ]);
-
+        var_dump($user);
+        //die;
         $token = $user->createToken('auth_token')->plainTextToken;
+        dd($token);
         return $this->sendResponse(['token' => $token, 'user' => $user->toArray()], 'User register successfully.');
     }
 
     public function login(Request $request)
     {
-        if (!Auth::attempt($request->only('email', 'password'))) {
-            return response()->json([
-                'message' => 'Invalid login details'
-            ], 401);
-        }
+        // if (!Auth::attempt($request->only('email', 'password'))) {
+        //     return response()->json([
+        //         'message' => 'Invalid login details'
+        //     ], 401);
+        // }
+
 
         $user = User::where('email', $request['email'])->firstOrFail();
 
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return $user->createToken($request->device_name)->plainTextToken;
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
